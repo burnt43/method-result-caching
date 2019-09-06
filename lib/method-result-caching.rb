731a9fe551eac05c92097833409dc6db35bf326b
burnt43 <jms.crsn@gmail.com>
2019-09-06 15:42:55 -0400

    replace question mark

--- lib/method-result-caching.rb
+++ lib/method-result-caching.rb
@@ -1,7 +1,8 @@
 module Kernel
   def cache_result(&block)
     method_name = caller_locations.first.label
-    instance_variable_name = "@#{method_name}".to_sym
+
+    instance_variable_name = "@#{method_name}".gsub('?', 'qmark').to_sym
 
     if instance_variable_defined?(instance_variable_name)
       instance_variable_get(instance_variable_name)
